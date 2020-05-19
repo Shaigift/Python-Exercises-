8c27d464f499265a831ac07a63401b9742713448
Shaigift <Mphogivenshai@gmail.com>
2020-05-19 09:32:47 +0200

    Add.

---
+++ Task Six.py
@@ -0,0 +1,12 @@
+numbers = input('Please insert numbers as comma separated numbers? : ')
+
+list = numbers.split(",")
+
+tuple = tuple(list)
+
+print("List : ",list)
+
+print("Tuple : ",tuple)
+
+
+
