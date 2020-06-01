8376fb4d9b900e06743556d3d86e9c1b665dd92c
Shaigift <Mphogivenshai@gmail.com>
2020-06-01 23:18:42 +0200

    w3resource

---
+++ Task Eighty-eight.py
@@ -0,0 +1,9 @@
+number = 245.788
+print('\nThe numbers are %f' % number)
+print('The numbers are %.2f' % number)
+
+order_amt = 212.374
+print('\nThe total order amount comes to %f' % order_amt)
+print('The total order amount comes to %.2f' % order_amt)
+print()
+
---
+++ Task Eighty-five.py
@@ -0,0 +1,7 @@
+numbers = [1,45,30,3,57,65,15]
+y = []
+for x in numbers:
+    if x % 15 == 0:
+        y.append(x)
+print(y)
+
